59c2e76d7e62a5866644da98c6550c3ef0020bea
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-24 16:59:22 -0300

    Seed congregations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,29 @@
-c1 = Circuit.find_or_create_by(name: 'RS-111')
-c2 = Circuit.find_or_create_by(name: 'RS-222')
+c1 = Circuit.find_or_create_by!(name: 'RS-111') do |c|
+  c.overseer_name = 'John Doe'
+  c.overseer_phone_number = '51 99999-8888'
+end
 
-c1.update(
-  overseer_name: 'John Doe',
-  overseer_phone_number: '51 99999-8888'
-)
+c2 = Circuit.find_or_create_by!(name: 'RS-222') do |c|
+  c.overseer_name = 'Jane Doe'
+  c.overseer_phone_number = '51 99999-7777'
+end
 
-c2.update(
-  overseer_name: 'James Doe',
-  overseer_phone_number: '51 99999-7777'
-)
+Congregation.find_or_create_by!(name: 'Sul', circuit_id: c1.id) do |c|
+  c.city_name = 'Nova York'
+end
+
+Congregation.find_or_create_by!(name: 'Norte', circuit_id: c1.id) do |c|
+  c.city_name = 'Nova York'
+end
+
+Congregation.find_or_create_by!(name: 'Sul', circuit_id: c1.id) do |c|
+  c.city_name = 'Nova Jersey'
+end
+
+Congregation.find_or_create_by!(name: 'Norte', circuit_id: c2.id) do |c|
+  c.city_name = 'California'
+end
+
+Congregation.find_or_create_by!(name: 'Sul', circuit_id: c2.id) do |c|
+  c.city_name = 'California'
+end
