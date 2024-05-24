d719a324e607204303fc4d30182f5d41e2c80d33
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-24 17:38:01 -0300

    Add address model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_193200) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_202729) do
+  create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "householder_name"
+    t.string "phone_number"
+    t.string "street_name"
+    t.string "number"
+    t.string "complement"
+    t.string "postal_code"
+    t.string "neighborhood"
+    t.string "city_name"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.bigint "congregation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["congregation_id"], name: "index_addresses_on_congregation_id"
+  end
+
   create_table "circuits", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "overseer_name"
@@ -35,4 +52,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_193200) do
     t.index ["number"], name: "index_congregations_on_number", unique: true
   end
 
+  add_foreign_key "addresses", "congregations"
 end
