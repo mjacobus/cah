99af07918b92a35eb36504796bdc52c2a4dc63ad
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-24 16:37:53 -0300

    Validate congregation

--- app/models/congregation.rb
+++ app/models/congregation.rb
@@ -1,4 +1,11 @@
 class Congregation < ApplicationRecord
-  validates :name, presence: true, uniqueness: { scope: :circuit_id }
-  validates :number
+  belongs_to :circuit
+
+  validates :name, presence: true, uniqueness: { scope: :circuit_id, case_sensitive: false }
+  validates :number, uniqueness: { case_sensitive: false }, if: -> { number.present? }
+  validates :circuit, presence: true
+
+  def to_s
+    name
+  end
 end
