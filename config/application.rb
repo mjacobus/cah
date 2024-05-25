ffa193044f7142a2b4d585000447cec3c0952119
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-24 21:12:34 -0300

    Do not generate helpers

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Cah
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.helper false
+    end
   end
 end
