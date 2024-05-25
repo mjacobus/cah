c436af1972245c6b824badd446fc1172171adc55
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-25 01:30:28 -0300

    Capture resolved and verified status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_202729) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_25_041843) do
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "householder_name"
     t.string "phone_number"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_202729) do
     t.bigint "congregation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "resolved", default: false, null: false
     t.index ["congregation_id"], name: "index_addresses_on_congregation_id"
   end
 
