2a276cc9c2e44c3fda4d0592cf4e13ae7b7fb3cd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-24 23:06:31 -0300

    Disable hotwire for now

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin 'application'
+# Disabling hotwire for now
+# pin "@hotwired/turbo-rails", to: "turbo.min.js"
