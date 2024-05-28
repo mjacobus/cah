c868358e3556573c82a8aebc92588d6277761a2a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-05-28 16:28:13 -0300

    Export button

--- app/packages/addresses/export_service.rb
+++ app/packages/addresses/export_service.rb
@@ -1,5 +1,21 @@
 module Addresses
   class ExportService
+    HEADERS = [
+      'Nome',
+      'Congregação',
+      'Telefone',
+      'Rua',
+      'Número',
+      'Complemento',
+      'Bairro',
+      'Cidade',
+      'CEP',
+      'Lat/Lon',
+      'Código',
+      'Atualizado',
+      'Resolvido'
+    ].freeze
+
     def initialize(one_sheet_per_congregation: false)
       @one_sheet_per_congregation = one_sheet_per_congregation
     end
@@ -8,7 +24,7 @@ module Addresses
       package = Axlsx::Package.new
       workbook = package.workbook
 
-      query_for(addresses:).find_each(batch_size: 20) do |congregation|
+      query_for(addresses:).find_each(batch_size: 50) do |congregation|
         sheet = worksheet_for(congregation, workbook:)
         add_addresses_to_worksheet(sheet:, congregation:, addresses:)
       end
@@ -16,39 +32,25 @@ module Addresses
       package
     end
 
-    # private
+    private
 
     def add_addresses_to_worksheet(sheet:, congregation:, addresses:)
-      sheet.add_row([
-        "Nome",
-        "Congregação",
-        "Telefone",
-        "Rua",
-        "Número",
-        "Complemento",
-        "Bairro",
-        "Cidade",
-        "CEP",
-        "Lat/Lon",
-        "Código",
-        "Atualizado",
-      ])
-
       congregation_addresses(congregation:, addresses:).find_each(batch_size: 100) do |address|
         sheet.add_row([
-          address.householder_name,
-          congregation.full_description,
-          address.phone_number,
-          address.street_name,
-          address.number,
-          address.complement,
-          address.neighborhood,
-          address.city_name,
-          address.postal_code,
-          address.geolocation.to_s,
-          address.id,
-          address.updated_at,
-        ])
+                        address.householder_name,
+                        congregation.full_description,
+                        address.phone_number,
+                        address.street_name,
+                        address.number,
+                        address.complement,
+                        address.neighborhood,
+                        address.city_name,
+                        address.postal_code,
+                        address.geolocation.to_s,
+                        address.id,
+                        address.updated_at,
+                        address.resolved? ? 'Sim' : 'Não'
+                      ])
       end
     end
 
@@ -64,13 +66,21 @@ module Addresses
     def worksheet_for(congregation, workbook:)
       if @one_sheet_per_congregation
         sheet = workbook.worksheets.find { |sheet| sheet.name == congregation.name }
-        sheet ||= workbook.add_worksheet(name: congregation.name)
+        sheet ||= workbook.add_worksheet(name: congregation.name).tap do |sheet|
+          add_headers_to_worksheet(sheet)
+        end
         return sheet
       end
 
       sheet = workbook.worksheets.first
-      sheet ||= workbook.add_worksheet(name: "Endereços")
+      sheet ||= workbook.add_worksheet(name: 'Endereços').tap do |sheet|
+        add_headers_to_worksheet(sheet)
+      end
       sheet
+    end
+
+    def add_headers_to_worksheet(sheet)
+      sheet.add_row(HEADERS)
     end
   end
 end
