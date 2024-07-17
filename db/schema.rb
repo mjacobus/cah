85c2bb5181777b8a04e661102618e359f40ed603
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-07-16 23:49:28 -0300

    Setup admin account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_212404) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_17_023344) do
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "householder_name"
     t.string "phone_number"
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_212404) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
