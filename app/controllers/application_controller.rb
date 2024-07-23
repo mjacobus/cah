314ebf761365cae6e47b46ad97c63ad6bf689614
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-07-23 07:04:35 -0300

    Add pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
   def set_timezone
     Time.zone = 'America/Sao_Paulo'
   end
+
+  def paginate(scope)
+    scope.page(params[:page]).per(per_page)
+  end
+
+  def per_page
+    number = params[:per_page].to_i
+    return 100 unless number.positive?
+
+    [number, 100].min
+  end
 end
