e3db9a410e7f454089729f4da05c2117314e8d11
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-07-28 11:17:56 -0300

    Leaner admin circuit page

--- app/admin/circuits.rb
+++ app/admin/circuits.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Circuit do
+  filter :name
+  filter :overseer_name
+  filter :overseer_phone_number
+  filter :created_at
+  filter :updated_at
+
   permit_params :name, :overseer_name, :overseer_phone_number
 end
