1e64e00e6259d7e5c5d0fea260ecc5650cb1211e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-08-05 16:46:16 -0300

    Sort export by code

--- app/packages/addresses/export_service.rb
+++ app/packages/addresses/export_service.rb
@@ -14,17 +14,14 @@ module Addresses
       'Data Atualização'
     ].freeze
 
-    def initialize(one_sheet_per_congregation: false)
-      @one_sheet_per_congregation = one_sheet_per_congregation
-    end
-
     def export_xls(addresses:)
       package = Axlsx::Package.new
       workbook = package.workbook
+      sheet = workbook.add_worksheet(name: 'Endereços')
+      add_headers_to_worksheet(sheet)
 
-      query_for(addresses:).find_each(batch_size: 50) do |congregation|
-        sheet = worksheet_for(congregation, workbook:)
-        add_addresses_to_worksheet(sheet:, congregation:, addresses:)
+      addresses.find_each(batch_size: 100) do |address|
+        add_address(sheet:, address:)
       end
 
       package
@@ -32,47 +29,20 @@ module Addresses
 
     private
 
-    def add_addresses_to_worksheet(sheet:, congregation:, addresses:)
-      congregation_addresses(congregation:, addresses:).find_each(batch_size: 100) do |address|
-        sheet.add_row([
-                        address.code_or_id,
-                        address.householder_name,
-                        address.congregation.full_description,
-                        address.phone_number,
-                        address.full_address,
-                        address.geolocation.to_s,
-                        address.human_stage_name,
-                        address.assignee_notes,
-                        format_date(address.expected_start_date),
-                        format_date(address.expected_finish_date),
-                        format_date(address.updated_at)
-                      ])
-      end
-    end
-
-    def query_for(addresses:)
-      congregation_ids = addresses.select(:congregation_id)
-      Congregation.where(id: congregation_ids).includes(:addresses)
-    end
-
-    def congregation_addresses(congregation:, addresses:)
-      congregation.addresses.where(id: addresses.select(:id)).ordered_by_code
-    end
-
-    def worksheet_for(congregation, workbook:)
-      if @one_sheet_per_congregation
-        sheet = workbook.worksheets.find { |sheet| sheet.name == congregation.name }
-        sheet ||= workbook.add_worksheet(name: congregation.name).tap do |sheet|
-          add_headers_to_worksheet(sheet)
-        end
-        return sheet
-      end
-
-      sheet = workbook.worksheets.first
-      sheet ||= workbook.add_worksheet(name: 'Endereços').tap do |sheet|
-        add_headers_to_worksheet(sheet)
-      end
-      sheet
+    def add_address(sheet:, address:)
+      sheet.add_row([
+                      address.code_or_id,
+                      address.householder_name,
+                      address.congregation.full_description,
+                      address.phone_number,
+                      address.full_address,
+                      address.geolocation.to_s,
+                      address.human_stage_name,
+                      address.assignee_notes,
+                      format_date(address.expected_start_date),
+                      format_date(address.expected_finish_date),
+                      format_date(address.updated_at)
+                    ])
     end
 
     def add_headers_to_worksheet(sheet)
